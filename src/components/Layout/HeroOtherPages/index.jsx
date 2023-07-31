d9e5dc528ad4aaa91bc680047856a6f85b1958f0
ctwc55 <ssy990408@gmail.com>
2023-08-01 01:43:52 +0900

    fix font size error of about-me page

--- src/components/Layout/HeroOtherPages/index.jsx
+++ src/components/Layout/HeroOtherPages/index.jsx
@@ -27,7 +27,7 @@ const HeroTextBox = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   row-gap: var(--gapRegular);
-  width: 530;
+  width: 530px;
   justify-items: ${({ centered }) => centered && 'center'};
   @media screen and (max-width: 767px) {
     width: 100%;
